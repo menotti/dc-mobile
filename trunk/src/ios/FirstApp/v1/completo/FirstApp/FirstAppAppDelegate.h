bf40ee6ec74b30cf62b606f2e47972d19f00d78c
regisrmz@gmail.com <regisrmz@gmail.com@4f1c0bd0-353d-d0c2-29ff-5190848f3017>
2013-06-04 01:48:23 +0000

    Código completo dos aplicativos de exemplo.

---
+++ trunk/src/ios/FirstApp/v1/completo/FirstApp/FirstAppAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  FirstAppAppDelegate.h
+//  FirstApp
+//
+//  Created by Regis Zangirolami on 08/05/13.
+//  Copyright (c) 2013 UFSCar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class FirstAppViewController;
+
+@interface FirstAppAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) FirstAppViewController *viewController;
+
+@end
---
+++ trunk/src/ios/FirstApp/v2/completo/FirstApp/SecondScreenViewController.m
@@ -0,0 +1,54 @@
+//
+//  SecondScreenViewController.m
+//  FirstApp
+//
+//  Created by Regis Zangirolami on 15/05/13.
+//  Copyright (c) 2013 UFSCar. All rights reserved.
+//
+
+#import "SecondScreenViewController.h"
+
+@interface SecondScreenViewController ()
+
+@end
+
+@implementation SecondScreenViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    
+    self.navigationController.title = @"Tela 2";
+    
+//    UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle:@"Voltar" style:UIBarButtonItemStyleBordered target:self action:@selector(backButtonTouched:)];
+//    self.navigationItem.backBarButtonItem = backButton;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)thirdScreen:(id)sender {
+    
+    // Cria próxima view
+    self.thirdScreen = [[ThirdScreenViewController alloc] initWithNibName:@"ThirdScreenViewController" bundle:nil];
+    
+    self.thirdScreen.textLabel = self.nameTextField.text;
+    
+    // Push da próxima view no Navigation Controller
+    [self.navigationController pushViewController:self.thirdScreen animated:YES];
+}
+
+@end
