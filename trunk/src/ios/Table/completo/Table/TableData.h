ffedc14872b30aee0888b260d07405ea94a64897
regisrmz@gmail.com <regisrmz@gmail.com@4f1c0bd0-353d-d0c2-29ff-5190848f3017>
2013-06-18 14:21:13 +0000

    Adicionado aplicativo de agenda.

---
+++ trunk/src/ios/Table/completo/Table/TableData.h
@@ -0,0 +1,15 @@
+//
+//  TableData.h
+//  Table
+//
+//  Created by Regis Zangirolami on 05/03/13.
+//  Copyright (c) 2013 Regis Zangirolami. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TableData : NSObject
+
+@property (nonatomic, copy) NSMutableArray *TableList;
+
+@end
---
+++ trunk/src/ios/Table/completo/Table/TableViewController.h
@@ -0,0 +1,27 @@
+//
+//  TableViewController.h
+//  Table
+//
+//  Created by Regis Zangirolami on 05/03/13.
+//  Copyright (c) 2013 Regis Zangirolami. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "DataContato.h"
+#import "Detail.h"
+
+@interface TableViewController : UIViewController <UITableViewDataSource,UITableViewDelegate,UISearchBarDelegate>
+
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (weak, nonatomic) IBOutlet UITableView *table;
+
+@property (nonatomic, retain) NSMutableDictionary *dictionary;
+@property (nonatomic, retain) NSMutableArray *keysArray;
+@property (nonatomic, retain) NSMutableArray *contatoObjArray;
+@property (nonatomic, retain) NSMutableArray *searchList;
+
+- (void) setDictionaryArray;
+- (void) sortObjArray:(NSMutableArray *)arrayObj;
+- (void) searchTable;
+
+@end
