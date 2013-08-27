27404a0d63f2010528a38b238278205ba0856cff
menotti <menotti@4f1c0bd0-353d-d0c2-29ff-5190848f3017>
2013-08-27 01:16:25 +0000

    Exemplo com JNI adicionado

---
+++ trunk/src/android/HelloNDK/src/com/example/hellondk/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.hellondk;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		Button button = (Button) findViewById(R.id.button1);
+		button.setOnClickListener(new View.OnClickListener() {
+		    public void onClick(View v) {
+				OlaLog("Isso vai aparecer no LogCat via chamada nativa...");
+		    }
+		});	
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	private native void OlaLog(String log);  
+
+	static {
+		System.loadLibrary("hello-ndk");
+	}
+}
