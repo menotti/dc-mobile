ed1acaf4f8b91d4f8f199bf4f3f22f908e35c9ab
caiop2007@gmail.com <caiop2007@gmail.com@4f1c0bd0-353d-d0c2-29ff-5190848f3017>
2014-11-27 00:06:47 +0000

    Pequena correção no xml da main do app de cardapio online e no método callServer.

--- trunk/doc/android/src/devapp/cardapio/callServer.java
+++ trunk/doc/android/src/devapp/cardapio/callServer.java
@@ -1,10 +1,11 @@
 @SuppressLint("NewApi")
-private void callServer(){
-	new Thread(){
-	public void run(){
-	String answer = 
-	ttpConnection.getSetDataWeb("http://mobile.dc.ufscar.br/backend/index.php");
-		degenerateJSON(answer);
-	}
-}.start();
+	private void callServer(){
+		new Thread(){
+ 
+public void run(){
+ String answer = 
+HttpConnection.getSetDataWeb("http://mobile.dc.ufscar.br/backend/index.php");
+				degenerateJSON(answer);
+		}
+	}.start();
 }
